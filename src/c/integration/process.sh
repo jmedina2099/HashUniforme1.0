932c6e16a73165e2da073c22638d7d8393254199
jmedina <jmedina@debian>
2020-10-17 23:42:40 -0500

    okoko

--- src/c/integration/process.sh
+++ src/c/integration/process.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
-OUT_PATH=$1
+OUT_DIR_HASH=$1
+OUT_DIR_HASH_XZ=$2
 TIMEFORMAT='TIME (USER)=%3lU'
 
-echo '===> PROCESANDO.. FILE=[['$2']]-BITS=[['$5']] OUT_DIR=[['$OUT_PATH']]'
+echo '===> PROCESANDO.. FILE=[['$3']]-BITS=[['$6']] OUT_DIR_HASH=[['$OUT_DIR_HASH']] OUT_DIR_HASH_XZ=[['$OUT_DIR_HASH_XZ']]'
 
 echo 'COUNTING BITS..'
-time nice -0 ../random/random $2 
+time nice -0 ../random/random $3 
 
 echo 'HASHING...'
-if [ $4 -eq 1 ]
+if [ $5 -eq 1 ]
 then
   echo -ne '\e[?7l'
 fi
 if [ $# -lt 5 ] 
 then
   echo 'cat is parsing file..'
-  cat $2 | while read line 
+  cat $3 | while read line 
   do
-    time nice -0 ./unit_test "$line" $3 $4 $5
+    time nice -0 ./unit_test "$line" $4 $5 $6
   done
 else
   echo 'c is parsing file..'
-  time nice -0 ./unit_test "" $3 $4 $5 $2 $7
+  time nice -0 ./unit_test "" $4 $5 $6 $3 $8
 fi
-if [ $4 -eq 1 ]
+if [ $5 -eq 1 ]
 then
   echo -ne '\e[?7h'
 fi
 echo 'HASHING IS DONE!'
 
 echo 'COUNTING BITS..'
-nice -0 ../random/random $7 
+nice -0 ../random/random $8 
 echo '----------|/'
 
 if [ $# -gt 6 ]
 then
   echo 'COMPRESSING (bz2)..'
-  time nice -0 tar cJf $OUT_PATH/$7.xz $7
+  time nice -0 tar cJf $OUT_PATH/$8.xz $8
   echo 'COUNTING BITS..'
-  time nice -0 ../random/random $7.xz
+  time nice -0 ../random/random $8.xz
 fi
 
-ls $7 $OUT_PATH/$7.xz -lh
-ls $7 $OUT_PATH/$7.xz -l
+ls $8 $OUT_PATH/$8.xz -lh
+ls $8 $OUT_PATH/$8.xz -l
 
-BITS_HASH=`stat -c %s $7`
-BITS_HASH_XZ=`stat -c %s $OUT_PATH/$7.xz`
+BITS_HASH=`stat -c %s $8`
+BITS_HASH_XZ=`stat -c %s $OUT_PATH/$8.xz`
 
 echo $BITS_HASH
 echo $BITS_HASH_XZ
