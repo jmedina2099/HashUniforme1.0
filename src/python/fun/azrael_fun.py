a8c6a5a275889f7bc0dd19c45e03ed87f4250bd2
jmedina <jmedina@debian>
2020-10-26 15:48:53 -0600

    python

--- src/python/fun/azrael_fun.py
+++ src/python/fun/azrael_fun.py
@@ -24,8 +24,16 @@ def evaluaFuncBool(char1,char2,char3,char4,char5):
     c_f = (( char1 + char2 ) & ( char3 + char4 ) + char5);
     return c_0+c_1+c_2+c_3+c_4+c_5+c_6+c_7+c_8+c_9+c_a+c_b+c_c+c_d+c_e+c_f;
 
-def pad(data):
+def conv(x):
+    return np.uint64(x);
 
+def rotate(x,o):
+    o %= 64;
+    x = conv(x);
+    o = conv(o);
+    return np.int64(x>>o);
+
+def pad(data):
     length = len(data);
     tail = length % 64;
     if (64 - tail >= 9):
@@ -79,15 +87,20 @@ class Azrael(object):
         char = [self._IV[0],self._IV[1],self._IV[2],self._IV[3],self._IV[4]];
         carrier = [self._IV[5],self._IV[6],self._IV[7],self._IV[8],self._IV[9]];
         
+        a =2;
+        b =3;
+        c =4;
+        d =5;
+        
         char[0] += input[length-2];
         char[1] += input[length-1];
         char[2] += input[0];
         char[3] += input[1];
         char[4] += input[2];
-        carrier[4] += carrier[3]; 
-        carrier[3] += carrier[2];
-        carrier[2] += carrier[1];
-        carrier[1] += carrier[0];
+        carrier[4] += rotate( carrier[3], d);
+        carrier[3] += rotate( carrier[2], c);
+        carrier[2] += rotate( carrier[1], b);
+        carrier[1] += rotate( carrier[0], a);
         carrier[0] += evaluaFuncBool( char[0],char[1],char[2],char[3],char[4]);
         
         for i in range(1,length-1):
@@ -96,10 +109,10 @@ class Azrael(object):
             char[2] += char[3];
             char[3] += input[i+1];
             char[4] += carrier[1];
-            carrier[4] += carrier[3]; 
-            carrier[3] += carrier[2];
-            carrier[2] += carrier[1];
-            carrier[1] += carrier[0];
+            carrier[4] += rotate( carrier[3], (8*(i+3)+d)); 
+            carrier[3] += rotate( carrier[2], (8*(i+2)+c));
+            carrier[2] += rotate( carrier[1], (8*(i+1)+b));
+            carrier[1] += rotate( carrier[0], (8*i+a));
             carrier[0] += evaluaFuncBool( char[0],char[1],char[2],char[3],char[4]);
             
         char[0] += carrier[0];
@@ -107,17 +120,17 @@ class Azrael(object):
         char[2] += char[3];
         char[3] += input[0];
         char[4] += carrier[1];
-        carrier[4] += carrier[3]; 
-        carrier[3] += carrier[2];
-        carrier[2] += carrier[1];
-        carrier[1] += carrier[0];
+        carrier[4] += rotate( carrier[3],(32+d) ); 
+        carrier[3] += rotate( carrier[2],(24+c) );
+        carrier[2] += rotate( carrier[1],(16+b) );
+        carrier[1] += rotate( carrier[0],(8+a) );
         carrier[0] += evaluaFuncBool( char[0],char[1],char[2],char[3],char[4]);
         
         carrier[0] += evaluaFuncBool( carrier[0],carrier[0],carrier[0],carrier[0],carrier[0]) + self._IV[0];
         carrier[1] += evaluaFuncBool( carrier[1],carrier[1],carrier[1],carrier[1],carrier[1]) + self._IV[1];
         carrier[2] += evaluaFuncBool( carrier[2],carrier[2],carrier[2],carrier[2],carrier[2]) + self._IV[2];
         carrier[3] += evaluaFuncBool( carrier[3],carrier[3],carrier[3],carrier[3],carrier[3]) + self._IV[3];
-        carrier[4] += evaluaFuncBool( carrier[4],carrier[4],carrier[4],carrier[4],carrier[4]) + self._IV[6];
+        carrier[4] += evaluaFuncBool( carrier[4],carrier[4],carrier[4],carrier[4],carrier[4]) + self._IV[3];
 
         mask1 = np.int64(0xffffffff);
         mask2 = np.int64(-0x1);
@@ -128,8 +141,8 @@ class Azrael(object):
             (((carrier[0]+carrier[1]+carrier[2]) << 16) & mask1) | \
             ((carrier[2]+carrier[3]+carrier[4]) & mask1);
 
-        hash += evaluaFuncBool( hash,hash,hash,hash,hash) + self._IV[4];
-        hash += evaluaFuncBool( hash,hash,hash,hash,hash) + self._IV[6];
+        hash += evaluaFuncBool( hash,hash,hash,hash,hash) + self._IV[2];
+        hash += evaluaFuncBool( hash,hash,hash,hash,hash) + self._IV[7];
         hash += length + 5 + 1 + 2;
         
         return longToBytes(hash);
@@ -138,7 +151,7 @@ class Azrael(object):
 def main():
     azrael = Azrael();
     output = azrael.getHashEval( bytearray(b'') );
-    hex = '0x' + ''.join('{:02x}'.format(i) for i in output);
+    hex = ''.join('{:02x}'.format(i) for i in output);
     print(hex);
     
     
